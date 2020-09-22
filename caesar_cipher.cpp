b54aa59f9b10f2e79f1eb5a9104c1ccd3bfabd53
Kartikey Tewari <kartikeya30@gmail.com>
2020-09-23 00:37:26 +0530

    add caesar cipher code

---
+++ caesar_cipher.cpp
@@ -0,0 +1,31 @@
+// Name: Kartikey Tewari
+// Roll No.: 17MI551
+// Computer Science and Engineering (Dual Degree)
+// National Institute of Technology, Hamirpur
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    int offset;
+    cout << "enter string= ";
+    cin >> s;
+    cout << "enter offser value= ";
+    cin >> offset;
+
+    int n=s.length();
+    for (int i=0;i<=n-1;i++)
+    {
+        int a=char(s[i]);
+        a+=offset;
+        if (a>=123)
+        {
+            a-=26;
+        }
+        cout << char(a);
+    }
+    cout << endl;
+
+    return 0;
+}
