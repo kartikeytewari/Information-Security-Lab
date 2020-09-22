71a4cd2856c918697853d9af6305ebcced9dd01c
Kartikey Tewari <kartikeya30@gmail.com>
2020-09-23 00:51:16 +0530

    add substitution cipher code

---
+++ cipher/substitution_cipher.cpp
@@ -0,0 +1,43 @@
+// Name: Kartikey Tewari
+// Roll No.: 17MI551
+// Computer Science and Engineering (Dual Degree)
+// National Institute of Technology, Hamirpur
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<char,char> subs_rule;
+    cout << "Enter substitution rule:" << endl;
+    int n;
+    cout << "enter number of rules= ";
+    cin >> n;
+
+    for (int i=0;i<=n-1;i++)
+    {
+        char a, b;
+        cout << "enter base character= ";
+        cin >> a;
+        cout << "enter final character= ";
+        cin >> b;
+        subs_rule[a]=b;
+    }
+
+    string s;
+    cout << "enter string= ";
+    cin >> s;
+
+    for (int i=0;i<=(int)s.length()-1;i++)
+    {
+        for (auto it=subs_rule.begin();it!=subs_rule.end();it++)
+        {
+            if (s[i]==it->first)
+            {
+                s[i]=it->second;
+            }
+        }
+    }
+
+    cout << "Final Text= " << s << endl;
+    return 0;
+}
