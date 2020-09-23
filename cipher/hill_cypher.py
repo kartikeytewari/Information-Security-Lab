9c1cfe8684da6b8ee511c7f0ce89c0287d831629
Kartikey Tewari <kartikeya30@gmail.com>
2020-09-24 01:51:33 +0530

    removed hill cypher implemetation in c++ and added in python because matrix management in C++ was cumbersome

---
+++ cipher/hill_cypher.py
@@ -0,0 +1,13 @@
+# Name: Kartikey Tewari
+# Roll No.: 17MI551
+# Computer Science and Engineering (Dual Degree)
+# National Institute of Technology, Hamirpur
+
+import numphy as np
+
+print("Enter 1 for encryption")
+print("Enter 2 for decryption")
+a=raw_input()
+
+if (a==1):
+    print("Enter string for encrypton:")
